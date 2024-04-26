5ae13a207a30979635512a92fdaca6904f81ebdf
mzfarshad <farshad.akbari.arzati@gmail.com>
2024-04-26 17:48:56 +0330

    created access mechanism

---
+++ cache/access.go
@@ -0,0 +1,95 @@
+package cache
+
+import (
+	"container/list"
+	"fmt"
+	"log"
+	"sync"
+)
+
+type CacheAccess interface {
+	Set(category, key string, val interface{})
+	Get(category, key string) (interface{}, error)
+	Delete(category, key string)
+}
+
+type cacheAccess struct {
+	data      map[string]map[string]*entryCache
+	evication *list.List
+	mutex     sync.Mutex
+}
+
+type entryCache struct {
+	value    interface{}
+	category string
+	key      string
+	elem     *list.Element
+}
+
+func (c *cacheAccess) NewCacheAccess() CacheAccess {
+	return &cacheAccess{
+		data:      make(map[string]map[string]*entryCache),
+		evication: list.New(),
+		mutex:     sync.Mutex{},
+	}
+}
+
+func (c *cacheAccess) Set(category, key string, val interface{}) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if _, ok := c.data[category]; !ok {
+		c.data[category] = make(map[string]*entryCache)
+		log.Printf("added new category: %s in cacheAccess", category)
+	}
+	if entry, ok := c.data[category][key]; ok {
+		entry.value = val
+		c.evication.MoveToFront(entry.elem)
+		log.Printf("Updated [%s][%s] : %v", category, key, val)
+	} else {
+		elem := c.evication.PushFront(&entryCache{
+			value:    val,
+			key:      key,
+			category: category,
+		})
+		c.data[category][key] = &entryCache{
+			value: val,
+			elem:  elem,
+		}
+		log.Printf("Successfully added [%s][%s]: %v in cacheAccess", category, key, val)
+	}
+
+}
+
+func (c *cacheAccess) Get(category, key string) (interface{}, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if entryes, ok := c.data[category]; ok {
+		if entry, ok := entryes[key]; ok {
+			c.evication.MoveToFront(entry.elem)
+			return entry.value, nil
+		} else {
+			return nil, fmt.Errorf("key not found")
+		}
+	}
+	return nil, fmt.Errorf("category not found")
+}
+
+func (c *cacheAccess) Delete(category, key string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if entryes, ok := c.data[category]; ok {
+		if entry, ok := entryes[key]; ok {
+			c.evication.Remove(entry.elem)
+			delete(c.data, entry.key)
+			log.Printf("Successfully deleted key : %s\n", key)
+			return
+		} else {
+			log.Printf("not found key : %s", key)
+			return
+		}
+	}
+	log.Printf("not found category : %s", category)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mzfarshad/cache-invalidation-mechanisms
+
+go 1.21.9
